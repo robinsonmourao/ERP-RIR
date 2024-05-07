f5147926c7d90ac8da8ea9a8693198256bced004
robinsonmourao <bob.info.guaratiba@gmail.com>
2024-05-07 19:05:22 -0300

    [conserto] links estavam acumulando com o path da tela atual [criadas] constantes para níveis de permissões 1, 2, 3 e 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
-class ApplicationController < ActionController::Base
+include Permissao
+
+class ApplicationController < ActionController::Base  
+
   protect_from_forgery with: :exception
   helper_method :current_usuario
 
---
+++ config/initializers/constants.rb
@@ -0,0 +1,6 @@
+module Permissao
+  SUPER = 1
+  ADMINISTRATIVO = 2
+  TECNICO = 3
+  CONSULTA = 4
+end
