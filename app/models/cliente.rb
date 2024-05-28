3a453c437c1965ce50e419a55bd7d24793a01d6b
robinsonmourao <bob.info.guaratiba@gmail.com>
2024-05-28 08:43:39 -0300

    [conserto] Relações da tabela 'cliente' [rollback] _form.html.erb de 'contatos' [identação] _form.html.erb e edit.html.erb de 'contatos' e new.html.erb de 'usuarios'

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -11,8 +11,8 @@ class Cliente < ApplicationRecord
   attribute :inscricao_estadual, :string
   attribute :inscricao_municipal, :string
 
-  has_many :contato, dependent: destroy
-  has_many :site, dependent: destroy
+  has_many :contato
+  has_many :site
 
   validates :nome_cliente, presence: true
   validates :cnpj, presence: true, uniqueness: true, length: { is: 14 }
