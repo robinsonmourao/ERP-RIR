5948607ccab1475b4c455bfb2d8871f4e4e071cd
robinsonmourao <bob.info.guaratiba@gmail.com>
2024-09-05 08:49:54 -0300

    [funcionalidade]descrições de cases detalhadas no terminal em formato <[xx_xx=>------...]> de profile com ou sem drop lines

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -15,3 +15,42 @@ end
 After('@apagar_tabela_principal_ao_terminar') do
   SetDown.remove_last_table
 end
+
+After do |scenario|
+  print '] '
+end
+
+Before do |scenario|
+  imprimir_com_quebra_linha = false;
+
+  feature_path = scenario.location.file
+  @feature_file_name = File.basename(feature_path)
+  @feature_folder_name = File.basename(File.dirname(feature_path))
+
+  $ID = extract_id_prefix.to_s
+
+  if ENV['TERMINAL_DROP_LINES_EACH_CASE'] == 'true'
+    print "\n[#{$ID}=>"
+  else
+    print "[#{$ID}=>"
+  end
+end
+
+def extract_id_prefix
+  folder_match = @feature_folder_name.match(/(Scenario)(\d+)/)
+  file_match = @feature_file_name.match(/id(\d+)(-|\.\d+)/)
+
+  folder_prefix = folder_match[2] ? folder_match[2] : "xx"
+
+  if file_match
+    if file_match[2] == '-'
+      file_prefix = file_match[1] || "xx"
+    else
+      file_prefix = @feature_file_name.match(/(\d+)(.)(\d+)/)
+    end
+  else
+    file_prefix = "xx"
+  end
+
+  "#{folder_prefix}_#{file_prefix}"
+end
